eb4f7915ca670b9fb174c5212fbcb477ba62ce49
masanuma <148186751+masanuma@users.noreply.github.com>
2025-06-19 17:01:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-
 import streamlit as st
-from ui.layout import render_layout
+from ui.layout import show_layout  # 関数名に合わせて修正
 
 st.set_page_config(page_title="InstaDish | 写真加工デモ版", layout="centered")
-render_layout()
+
+show_layout()  # 正しい関数を呼び出し
