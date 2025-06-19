f692ccee5a90161c1647f1d44d66c3234bcf4f74
masanuma <148186751+masanuma@users.noreply.github.com>
2025-06-19 16:39:42 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 
 import streamlit as st
-from ui import render_main_ui
+from ui.layout import render_layout
 
-# ページ設定
 st.set_page_config(page_title="InstaDish | 写真加工デモ版", layout="centered")
-
-# カスタムCSS適用
-with open("theme.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-# UIレンダリング
-render_main_ui()
+render_layout()
