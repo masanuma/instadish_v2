80260afa9d43492e9fdac7c8f05d67da91c8457a
masanuma <148186751+masanuma@users.noreply.github.com>
2025-06-19 16:33:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-# Main app file
+
 import streamlit as st
-st.write('App Placeholder')
+from ui import render_main_ui
+
+# ページ設定
+st.set_page_config(page_title="InstaDish | 写真加工デモ版", layout="centered")
+
+# カスタムCSS適用
+with open("theme.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# UIレンダリング
+render_main_ui()
