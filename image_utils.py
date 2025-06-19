db5ef33644b376614d5736c652f1108322be8a96
masanuma <148186751+masanuma@users.noreply.github.com>
2025-06-19 15:27:29 +0900

    Delete image_utils.py

--- image_utils.py
+++
@@ -1,8 +0,0 @@
-
-from PIL import ImageEnhance
-
-def process_image(image):
-    image = ImageEnhance.Brightness(image).enhance(1.2)
-    image = ImageEnhance.Contrast(image).enhance(1.3)
-    image = ImageEnhance.Sharpness(image).enhance(2.0)
-    return image
