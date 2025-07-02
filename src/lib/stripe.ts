5f8f8a345807b341234f11348298ef5a7494b993
masanuma <masanuma@users.noreply.github.com>
2025-07-02 18:21:19 +0900

    Stripe apiVersionを2025-06-30.basilに更新（型エラー修正）

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 
 // サーバーサイド用Stripeクライアント
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-05-28.basil',
+  apiVersion: '2025-06-30.basil',
 })
 
 // クライアントサイド用Stripe設定
